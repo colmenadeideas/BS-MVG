ff463381fc1baa8307b941b1a0b273644a7186db
dlarez <dlarez@besign.com.ve>
2016-02-25 07:29:25 -0430

    Front

--- app/views/default/footer-site.php
+++ app/views/default/footer-site.php
@@ -1,4 +1,4 @@
-	<footer class="col-lg-12 text-center fixedfooter">
+	<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center fixedfooter">
 		<div class="col-lg-4 col-md-4 col-sm-4"><a href="tel://+582418257693"><i class="fa fa-phone"></i> 0241 825 7693</a></div>
 		<div class="col-lg-4 col-md-4 col-sm-4"><small>Model's View Group C.A. J-31701084-1</small></div>
 		<div class="col-lg-4 col-md-4 col-sm-4">C.C. GUAPARO - VALENCIA, CARABOBO<br></div>
